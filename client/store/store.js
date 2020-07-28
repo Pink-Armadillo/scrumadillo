c2d9440bf1fb885ba925e35edb8764af98141bcb
Kevin Fey <62725736+kevinfey@users.noreply.github.com>
2020-07-28 08:10:35 -0700

    Update store.js
    More comments

--- client/store/store.js
+++ client/store/store.js
@@ -7,12 +7,18 @@ import cardReducer from '../reducers/cardSlice';
 import deckReducer from '../reducers/deckSlice';
 import loginReducer from '../reducers/loginSlice';
 
+//Middleware for logging state with every state change
+
 const logger = (store) => (next) => (action) => {
   console.log('dispatching', action);
   let result = next(action);
   console.log('next state', store.getState().card);
   return result;
 };
+
+//Middleware that sends the entire state of the app up to Mongo DB with every state change.
+//Currently this just overwrites a users board state but the goal was to create an array of board states
+// so the user could time travel even after logging out or closing the page
 
 const updateBoardServer = (store) => (next) => (action) => {
   let payload = store.getState();
@@ -32,8 +38,8 @@ const updateBoardServer = (store) => (next) => (action) => {
   return result;
 };
 
-//user the name of the reducer to access that piece state in the global environment
-//global state is an object containing 3 reducers store = {card{}, deck{}, login{}}
+//use the name of the reducer to access that piece state in the global environment
+//global state is an object containing 3 reducers. store = {card{}, deck{}, login{}}
 
 const store = configureStore({
   reducer: combineReducers({
